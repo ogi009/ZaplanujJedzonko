17d3d6780caec3408fb04882bc44fdb4b745fee4
Vearso <gus.szymon@gmail.com>
2020-10-12 20:43:00 +0200

    Cleaning code in banner implementation

--- js/app.js
+++ js/app.js
@@ -1,34 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const banner = () => {
         const arrowLeft = document.querySelector(".banner .fa-arrow-left");
         const arrowRight = document.querySelector(".banner .fa-arrow-right");
         const banner = document.querySelector(".banner_img");
 
         const moveBanner = (target) => {
             if (banner.scrollLeft === 0) {
-                if (target.className.includes('right')) {
-                    banner.scrollTo({left: 1440, behavior: 'smooth'})
-                } else {
-                    banner.scrollTo({left: 2880, behavior: 'smooth'})
-                }
-            } else if (banner.scrollLeft === 1440) {
-                if (target.className.includes('right')) {
-                    banner.scrollTo({left: 2880, behavior: 'smooth'})
-                } else {
-                    banner.scrollTo({left: 0, behavior: 'smooth'})
-                }
-            } else if (banner.scrollLeft === 2880) {
-                if (target.className.includes('right')) {
-                    banner.scrollTo({left: 0, behavior: 'smooth'})
-                } else {
-                    banner.scrollTo({left: 1440, behavior: 'smooth'})
-                }
-            }
+                target.className.includes('right') ? banner.scrollTo({left: 1440, behavior: 'smooth'})
+                    : banner.scrollTo({left: 2880, behavior: 'smooth'})
 
+            } else if (banner.scrollLeft === 1440) {
+                target.className.includes('right') ? banner.scrollTo({left: 2880, behavior: 'smooth'})
+                    : banner.scrollTo({left: 0, behavior: 'smooth'})
+
+            } else if (banner.scrollLeft === 2880) {
+                target.className.includes('right') ? banner.scrollTo({left: 0, behavior: 'smooth'})
+                    : banner.scrollTo({left: 1440, behavior: 'smooth'})
+            }
         }
+
         arrowLeft.addEventListener('click', e => moveBanner(e.target));
         arrowRight.addEventListener('click', e => moveBanner(e.target));
-    }
-    banner();
+
 });
