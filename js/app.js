3ca1ec3243c7ef1c560b56713b75e61301a16d19
Vearso <gus.szymon@gmail.com>
2020-10-12 20:35:05 +0200

    Banner sliding interactively

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-const banner = () => {
-    const arrowLeft = document.querySelector(".banner .fa-arrow-left");
-    const arrowRight = document.querySelector(".banner .fa-arrow-right");
-    const banner = document.querySelector(".banner_img");
+    const banner = () => {
+        const arrowLeft = document.querySelector(".banner .fa-arrow-left");
+        const arrowRight = document.querySelector(".banner .fa-arrow-right");
+        const banner = document.querySelector(".banner_img");
 
-    arrowLeft.addEventListener('click', e => {
-        banner.scrollLeft = banner.children[0].width;
-    })
-}
-banner();
+        const moveBanner = (target) => {
+            if (banner.scrollLeft === 0) {
+                if (target.className.includes('right')) {
+                    banner.scrollTo({left: 1440, behavior: 'smooth'})
+                } else {
+                    banner.scrollTo({left: 2880, behavior: 'smooth'})
+                }
+            } else if (banner.scrollLeft === 1440) {
+                if (target.className.includes('right')) {
+                    banner.scrollTo({left: 2880, behavior: 'smooth'})
+                } else {
+                    banner.scrollTo({left: 0, behavior: 'smooth'})
+                }
+            } else if (banner.scrollLeft === 2880) {
+                if (target.className.includes('right')) {
+                    banner.scrollTo({left: 0, behavior: 'smooth'})
+                } else {
+                    banner.scrollTo({left: 1440, behavior: 'smooth'})
+                }
+            }
+
+        }
+        arrowLeft.addEventListener('click', e => moveBanner(e.target));
+        arrowRight.addEventListener('click', e => moveBanner(e.target));
+    }
+    banner();
 });
