727e694677aa70d0468fff62f2f8b948ba2f87e4
KrynickiJarek <krynickijarek@gmail.com>
2020-10-14 16:02:35 +0200

    grid functionality in planing deskop

--- js/app.js
+++ js/app.js
@@ -1,25 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const message = document.querySelector('#welcome_message')
     const input = document.querySelector("#name_input");
-    const button = document.querySelector("#name_submit");
+    const form = document.querySelector("#form");
     const user = document.querySelector("#user_name");
+    const gridPlan = document.querySelector(".grid_plan");
 
     let userName;
-    if(localStorage.getItem('name' !== null)) {
+    if (localStorage.getItem('name' !== null)) {
         userName = localStorage.getItem('name');
         message.style.display = 'none';
-    }
-    else {
-        button.addEventListener('click', e => {
+        user.innerText = userName;
+    } else {
+        form.addEventListener('submit', e => {
+            e.preventDefault();
             if (input.value !== null && input.value !== '') {
                 userName = input.value;
                 localStorage.setItem('name', userName);
+                user.innerText = userName;
+                message.classList.add("d_none")
+                gridPlan.classList.remove("d_none")
+
             } else {
                 input.style.borderColor = '$color';
                 input.style.placeholder = 'Proszę wprowadzić imię';
                 input.style.placeholder.color = 'tomato';
+                user.innerText = userName;
             }
         })
     }
-    user.innerText = userName;
 });
