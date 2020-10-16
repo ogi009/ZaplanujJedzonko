60348321069f5c64e252608103df007342eb0fa0
Vearso <gus.szymon@gmail.com>
2020-10-16 23:45:30 +0200

    Created styles for recipes - list

--- js/navigation.js
+++ js/navigation.js
@@ -7,10 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const recipes = document.getElementById('recipes_panel');
     const mainPanel = document.querySelector(".grid_plan");
     const schedulePanel = document.querySelector(".schedule_plan");
-    // const recipePanel = document.querySelector(".container_recipe");
+    const recipesList = document.querySelector(".recipes_list");
     const gridPlan = document.querySelector(".grid_plan");
     const gridPlan2 = document.querySelector(".grid_plan2");
-   // const recipePanel = document.querySelector(".container_recipe");
     const scheduleList = document.querySelector(".schedule_list");
     const addSchedule = document.querySelector(".schedule_list-add");
     const saveButton =document.querySelector(".schedule_plan-save");
@@ -22,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
         e.preventDefault();
         mainPanel.classList.remove('d_none');
         schedulePanel.classList.add('d_none');
-        //recipePanel.classList.add('d_none');
+        recipesList.classList.remove('d_none');
         scheduleList.classList.add('d_none')
         // recipePanel.classList.add('d_none');
 
@@ -33,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
         mainPanel.classList.add('d_none');
         schedulePanel.classList.add('d_none');
         scheduleList.classList.remove('d_none')
+        recipesList.classList.add('d_none');
         scheduleListActions();
 
     });
@@ -41,10 +41,10 @@ document.addEventListener("DOMContentLoaded", function() {
         e.preventDefault();
         mainPanel.classList.add('d_none');
         schedulePanel.classList.add('d_none');
-        //recipePanel.classList.remove('d_none');
+        recipesList.classList.remove('d_none');
         scheduleList.classList.add('d_none')
-        // recipePanel.classList.remove('d_none');
     });
+
     addSchedule.addEventListener('click', e=> {
         e.preventDefault();
         mainPanel.classList.add('d_none');
