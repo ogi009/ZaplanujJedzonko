dab3fba4b08519994afdb75be0a5f4b77ef84656
ogi009 <70148652+ogi009@users.noreply.github.com>
2020-10-16 21:57:10 +0200

    adding widgets

--- js/navigation.js
+++ js/navigation.js
@@ -5,25 +5,29 @@ document.addEventListener("DOMContentLoaded", function() {
     const recipes = document.getElementById('recipes_panel');
     const mainPanel = document.querySelector(".grid_plan");
     const schedulePanel = document.querySelector(".schedule_plan");
-    const recipePanel = document.querySelector(".container_recipe");
+    // const recipePanel = document.querySelector(".container_recipe");
+    const gridPlan = document.querySelector(".grid_plan");
+    const gridPlan2 = document.querySelector(".grid_plan2");
     main.addEventListener('click' , e=> {
         e.preventDefault();
         mainPanel.classList.remove('d_none');
         schedulePanel.classList.add('d_none');
-        recipePanel.classList.add('d_none');
+        // recipePanel.classList.add('d_none');
+
+        gridPlan2.classList.add("d_none");
     });
     plans.addEventListener('click', e => {
         e.preventDefault();
         mainPanel.classList.add('d_none');
         schedulePanel.classList.remove('d_none');
-        recipePanel.classList.add('d_none');
+        // recipePanel.classList.add('d_none');
     });
 
     recipes.addEventListener('click', e => {
         e.preventDefault();
         mainPanel.classList.add('d_none');
         schedulePanel.classList.add('d_none');
-        recipePanel.classList.remove('d_none');
+        // recipePanel.classList.remove('d_none');
     });
 
 });
--- js/newRecipt.js
+++ js/newRecipt.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const addNewRecipt = document.querySelector(".addNewRecipt");
+    const addNewPlan = document.querySelector(".addNewPlan");
     const gridPlan = document.querySelector(".grid_plan");
     const gridPlan2 = document.querySelector(".grid_plan2");
+    const schelduePlan = document.querySelector(".schedule_plan");
 
     addNewRecipt.addEventListener("click", e => {
         e.preventDefault();
@@ -10,4 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
         gridPlan2.classList.remove("d_none");
     })
 
+    addNewPlan.addEventListener("click", e=> {
+        e.preventDefault();
+        gridPlan.classList.add("d_none");
+        schelduePlan.classList.remove("d_none");
+
+    })
 });
